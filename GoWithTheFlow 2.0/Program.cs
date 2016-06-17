15d1cc0077c488f937064958c20bf94a4fc55750
Echarger1 <echarger1@gmail.com>
2016-06-16 21:21:36 -0400

    Added Questions

--- GoWithTheFlow 2.0/Program.cs
+++ GoWithTheFlow 2.0/Program.cs
@@ -10,16 +10,45 @@ namespace GoWithTheFlow_2._0
     {
         static void Main(string[] args)
         {
+           
+
             Console.WriteLine("Is the baby crying?");
 
             string answer = Console.ReadLine();
 
-            while (!(answer.ToLower == "yes") || (answer.ToLower == "no"))
+            while (!(answer.ToLower() == "yes") && (answer.ToLower() == "no"))
             {
-                Console.WriteLine. ("Type Yes or No");
+                Console.WriteLine("Type Yes or No");
                 answer = Console.ReadLine();
             }
 
+
+            if (answer.ToLower() == "yes")
+            {
+                Console.WriteLine("Has baby eaten recently?");
+                Console.ReadLine();
+
+                if (answer.ToLower() == "yes")
+                {
+                    Console.WriteLine("Does baby stink?");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Feed baby.");
+                    Console.ReadLine();
+                }
+            
+
+                
+            }
+            else
+            {
+                Console.WriteLine("Do nothing.");
+               
+            }
+
+            
             Console.ReadKey();
         }
     }
